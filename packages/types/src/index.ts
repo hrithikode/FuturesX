6fa58db8f4c8a4b7d5eeacfbd1dc410809767c23
hrithik <hrithikmails88@gmail.com>
2026-04-22 23:49:17 +0530

    added types

---
+++ packages/types/src/index.ts
@@ -0,0 +1,74 @@
+export type Side = 'buy' | 'sell';
+export type OrderType = 'limit' | 'market';
+export type OrderStatus = 'open' | 'partially_filled' | 'filled' | 'cancelled';
+
+export interface Order {
+    orderId: string;
+    userId: string;
+    market: string;     
+    type: OrderType;
+    price: number;        
+    quantity: number;
+    filledQuantity: number;
+    status: OrderStatus;
+    createdAt: number;  
+}
+
+export interface Trade {
+    tradeId: string;
+    market: string;
+    price: number;
+    quantity: number;
+    makerOrderId: string;
+    takerOrderId: string;
+    timestamp: number;
+}
+
+
+export interface CreateOrderMessage {
+    type: 'CREATE_ORDER';
+    data: {
+        orderId: string; 
+        userId: string;
+        market: string;
+        price: number;
+        quantity: number;
+        side: Side;
+        orderType: OrderType;
+    }
+}
+
+export interface CancelOrderMessage {
+    type: 'CANCEL_ORDER';
+    data: {
+        orderId: string;
+        market: string;
+    }
+}
+
+export type EngineMessage = CreateOrderMessage | CancelOrderMessage;
+
+
+export interface TradeExecutedEvent {
+    type: 'TRADE_EXECUTED';
+    data: {
+        tradeId: string;
+        market: string;
+        price: number;
+        quantity: number;
+        makerOrderId: string;
+        takerOrderId: string;
+        timestamp: number;
+    }
+}
+
+export interface OrderUpdateEvent {
+    type: 'ORDER_UPDATE';
+    data: {
+        orderId: string;
+        userId: string;
+        market: string;
+        status: OrderStatus;
+        filledQuantity: number;
+    }
+}

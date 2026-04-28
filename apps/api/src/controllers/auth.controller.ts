aa97ec1ebe651fbb46c070e793e2fe9d128d9d4d
hrithik <hrithikmails88@gmail.com>
2026-04-28 18:19:17 +0530

    added login and logout endpoints

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { prisma } from "@repo/prisma";
-import { registerSchema } from "../schema/auth.types";
+import { loginSchema, registerSchema } from "../schema/auth.types";
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt'
 
@@ -65,9 +65,66 @@ export const register = async (req: Request, res: Response) => {
 
 
 export const login = async (req: Request, res: Response) => {
+    try{
+        const user = loginSchema.safeParse(req.body);
 
+        if(!user.success) {
+            return res.status(400).json({error: "credentials are required"});
+        }
+
+        const { email, password } = user.data;
+
+        if(!email ||  !password) {
+            return res.status(400).json({error: "email and password are required"});
+        }
+
+        const loginUser = await prisma.user.findUnique({
+            where: {
+                email
+            },
+        });
+
+        if (!loginUser) {
+            return res.status(404).json({error: "user not found"});
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, loginUser.password)
+
+        if(!isPasswordValid) {
+            return res.status(401).json({ error: "password is wrong"});
+        }
+
+        const token = jwt.sign({id: loginUser.id, email: loginUser.email}, process.env.JWT_SECRET!, {expiresIn: "1h"});
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            sameSite: "none",
+            secure: true,
+            maxAge: 60 * 60 * 1000
+        });
+
+        res.json({
+            message: "User logged In successfully",
+            user: { 
+                id: loginUser.id,
+                email: loginUser.email,
+                name: loginUser.name
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({ error: "Internal server error"});
+    }
 };
 
 export const logout = async (req: Request, res: Response) => {
-
+    try{
+        res.clearCookie("token", {
+            httpOnly: true,
+            sameSite: "none",
+            secure: true
+        });
+        res.json({ message: "logout successfully" });
+    } catch (error) {
+        res.status(500).json({error: " Internal server error "})
+    }
 };
